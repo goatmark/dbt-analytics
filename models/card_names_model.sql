cd0627ce47d240b1c6bbcf9074b42bb3191ca40d
Mark Khoury <mark@markkhoury.me>
2025-08-11 22:06:43 +0000

    initial card models

---
+++ models/card_names_model.sql
@@ -0,0 +1,7 @@
+select 3221 card_last4, 'Chase' bank, 'Freedom' card_name
+union all
+select 6823 card_last4, 'Chase' bank, 'Prime' card_name
+union all
+select 4245 card_last4, 'Chase' bank, 'Sapphire' card_name
+union all
+select 5083 card_last4, 'Wells Fargo' bank, 'Bilt' card_name
